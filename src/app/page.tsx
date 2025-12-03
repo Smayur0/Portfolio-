c7b641117cd09bff66348928bba2058d29054e25
Mayurs0 <mayurs@iconnectsolutions.com>
2025-12-03 12:49:35 +0530

    [FEAT]:Next.js tailwind and shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className=" flex flex-col h-screen mx-auto bg-neutral-400 max-w-4xl">
+        hello
+        <Button variant={"secondary"}>Click me</Button>
+      </div>
+    </div>
+  );
+}
